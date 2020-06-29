d3a13f4927f8b288c421f23186ac829e3a37a611
Ayoola David <ayooladavid@gmail.com>
2020-06-29 22:24:26 +0100

    Final changes

--- Sally/js/script.js
+++ Sally/js/script.js
@@ -23,65 +23,6 @@ gsap.from(`.hero-content`, 2,{ opacity:0, x: -70} )
 gsap.from(`.phone`, 1.75,{ opacity:0, delay: .5, ease: "power3.out"} )
 gsap.from(`.changer`, 1.5,{ opacity:0, delay: 0.75,  y: -20, ease: "power3.out"} )
 
-
-const navigator = (i) => {
-    console.log("click click")
-            gsap.to(`#day-3`, .5,{attr:{transform: paths[i].rect_position}})
-            gsap.to(`#PATH_1-3`, 1,{attr:{
-                d: paths[i].line_path,
-                transform: paths[i].line_transform
-            }})
-            gsap.from(`#Path_6-2`, 1,{ opacity:0, y: -10, x: 0, y: 0,} )
-            gsap.to(`#Path_6-2`, 1,{ ease: "power3.out",
-                    attr:{
-                        d: paths[i].shadow_path,
-                        transform: paths[i].shadow_transform
-                    }})
-            gsap.to(`#linear-gradient`, 1.5,{
-                attr:{
-                    x1: paths[i].circle_path[0], 
-                    y1: paths[i].circle_path[1], 
-                    x2: paths[i].circle_path[2], 
-                    y2: paths[i].circle_path[3]
-                }})
-            gsap.to(`#cursor_group`, 1,{attr:{transform: paths[i].cursor_path}})
-
-            gsap.to(title,1,{
-               text: {
-                    value: paths[i].title,
-                    delimiter: " "
-                }
-            })
-            gsap.to(hero_text,1,{
-                text: {
-                    value: paths[i].text,
-                    delimiter: " "
-                }
-            })
-
-                gsap.to(`#${changers[i].id}`,.75,{className:`+=active r${i+1}`})
-            for(let j = 0; i < changers.length; j++){
-                if(changers[j] != changers[i]){
-                    gsap.to(`#${changers[j].id}`,.75,{className:`+=not_active r${j+1}`})
-                }
-            }
-}
-
-//large screen navigator
-    for(let i = 0; i < changers.length; i++) {
-        let changer = changers[i]
-        changer.onclick  = () => {
-            changers[i].setAttribute("ok", "1")
-            for(item of changers){
-                if(item != changers[i]){
-                    item.setAttribute("ok", "0")
-                }
-            }
-            navigator(i)
-
-        }
-    }
-
     //small screen menu bar
     const tween = gsap.from(".mobile", 0.5, {
         opacity: 0,
